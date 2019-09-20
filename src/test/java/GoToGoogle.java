80450ad492c82027d0bf41583ac7a0a32d52808b
s.nedaikhleb <s.nedaikhleb@hexa.com.ua>
2019-09-20 19:11:38 +0300

    f2

--- src/test/java/GoToGoogle.java
+++ src/test/java/GoToGoogle.java
@@ -1,11 +1,35 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class GoToGoogle {
 
     public static void main(String[] args) {
 
-        WebDriver dr = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        DesiredCapabilities caap = new DesiredCapabilities();
+        caap.setCapability(ChromeOptions.CAPABILITY, options);
+
+        WebDriver dr = new ChromeDriver(caap);
+        dr.navigate().to("https://www.google.com");
+
+        System.out.println("This is: "+dr.getTitle());
+        System.out.println("*****************");
+
+        WebElement searchField = dr.findElement(By.xpath("//div[@class='SDkEP']//input[@type='text']"));
+        searchField.sendKeys("udemy");
+
+        dr.findElement(By.xpath("//div[@class='FPdoLc VlcLAe']//input[@class='gNO89b']")).submit();
+
+     //   System.out.println("i can see"+ dr.findElements(By.xpath("//div[@class='r']"))+" answers");
+
+
+        //dr.quit();
 
 
     }
